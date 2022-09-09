eb6383719371ebe7739633ce0ad6dc4e0b0c6f2e
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-08 22:31:35 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
 /..
-. main - print the string in the put function
+.main - print the string in the put function
 .
+. Description: using the main function
+.this program prints "programming is like a multiligual puzzle
+. Resturn: 0
+./
+int main(void)
+{
+	printf("with proper gramar, but the outcome is a piece of art,/n");
+	return (0) ;
+}
 
- 
-[A
