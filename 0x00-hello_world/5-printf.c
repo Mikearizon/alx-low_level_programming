5c5c3b5020af1033c17ec7589fd95d31ab12167a
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-09 04:27:39 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,14 @@
-
 #include <stdio.h>
 
-/:
-. main - print the string in the put function
-.
-. Description: using the main function
-. this progam prints "programming is like a multiligual puzzle"
-. Return: 0
-./
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function 
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
 {
-    printf("with proper grammer, but the outcome is a piece of art, /n");
-    return (0);
+	prints("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
