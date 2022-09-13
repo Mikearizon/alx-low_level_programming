8a6feea6e16c704f89e828dcf0673c4a1b0e0446
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-13 14:21:51 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-a script in the header file
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_HINCLUDE
+
+int _putcher(char);
+void print_alphabet(void);
+void print_alphabet-x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(in r, int n);
+
+#endif
