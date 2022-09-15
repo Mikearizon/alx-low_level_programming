2e2b1bb32c03ca7ec55f6a91cbcfd1633900058e
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-15 13:07:54 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,12 +2,15 @@
 
 /**
 * main - tests function tha prints if integer is positive or negative
-* Return: o
+* Return: 0
 */
+
 int main(void)
 {
 int i;
-i = o;
+
+i = 0;
 positive_or_negative(i);
+
 return (0);
 }
