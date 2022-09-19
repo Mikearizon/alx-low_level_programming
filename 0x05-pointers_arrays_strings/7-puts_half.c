d92a5c60e538ab1e0ed2a7579a694c64e761c99c
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-19 15:24:17 -0700

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
+
+
 /**
 * puts_half - prints a string
 * @str: string to print
 *
-* Return: nothing
+ * Return: nothing
 */
 
 void puts_half(char *str)
 {
 int i, len = _strlen(str);
-for (i = ((len - 1) / 2) + 1; i < len; i++
+
+for (i = ((len - 1) / 2) + 1; i < len; i++)
 putchar(*(str + i));
 putchar(10);
 }
