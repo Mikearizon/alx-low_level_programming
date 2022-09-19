f48beae0794a251826f25bcfabdffc8ef270594d
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-19 09:03:38 -0700

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
-*_Strlen - returns the length of a string
+*_strlen - returns the length of a string
 *@s: string
 *Return: returns length as integer;
 */
 
-int _Strlen(char *s)
+int _strlen(cahr *s)
 {
 int len = 0;
 while (*(s + len) != '\0')
 len++;
 
 return (len);
-}
