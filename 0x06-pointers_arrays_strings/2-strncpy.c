af517936930f6a6ad203c394cb2c025000870b96
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-09-21 14:02:56 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
-*_strncpy -c function that copies a string, including the
+*_strncpy -c function that copies a string, including the 
 *terminating null byte, using at most an inputted number of bytes.
-*If the length of the source string is lessthan the maxmum byte number
+*If the length of the source string is less than the maximum byte number,
 *the remainder of the destination string is filled with null bytes.
-*works identically to the standard libraray function 'strncpy'.
+*works identically to the standard library function 'strncpy'.
 *@dest: buffer storing the string copy
-*@src:the source string
+*@src: the source string
 *@n: max number of byte copied
 *Return: returns
 */
-
-char *_strncpy(char *dest, Char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 int i;
 for (i = 0; i < n && src[i] != '\0'; i++)
