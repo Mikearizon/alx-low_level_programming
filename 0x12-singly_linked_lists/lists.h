74b0d4c26d163bb339e3c55cc9febe8f94b12dd5
Mikearizongit confi --global user.name Mikearizon <michealarinze1@gmail.comgit config --global user.name mikearizon>
2022-10-21 15:35:09 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,11 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 /**
  * struct list_s - singly linked list
@@ -27,5 +29,6 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *str);
+
 
 #endif /*LISTS_H*/
